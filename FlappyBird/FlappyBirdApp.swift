4db78211976b6fd4c8bd16b1deb9049802154807
Matthew Zhang <gzhang@lcs.on.ca>
2024-01-18 17:19:14 -0500

    Initial Commit

---
+++ FlappyBird/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird
+//
+//  Created by GengYu Zhang on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
